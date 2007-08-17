f8c9793dc6db79ab09efb2c7b990600ad16e13e7
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-08-17 01:48:21 +0000

    Generate config/deploy.rb... not Capfile

--- generators/lwt_deployment/lwt_deployment_generator.rb
+++ generators/lwt_deployment/lwt_deployment_generator.rb
@@ -1,7 +1,8 @@
 class LwtDeploymentGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      m.template 'Capfile', 'Capfile'
+      m.directory 'config'
+      m.template 'config/deploy.rb', 'config/deploy.rb'
     end
   end
 end
