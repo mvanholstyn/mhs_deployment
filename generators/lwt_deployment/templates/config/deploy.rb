165569e55ca9e7bdf7e1aae1d3a36cc6cb638287
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-10-09 19:16:14 +0000

    Remove dumb comments

--- generators/lwt_deployment/templates/config/deploy.rb
+++ generators/lwt_deployment/templates/config/deploy.rb
@@ -3,9 +3,6 @@ load File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'lwt_deploymen
 set :application, "example"
 set :repository_host, "svn.example.com"
 
-# Used for web.disable and web.enable
 role :web, "example.com"
-# Used for deploy.start, deploy.stop, deploy.restart
 role :app, "example.com"
-# Used for deploy.migrate
 role :db, "example.com", :primary => true
