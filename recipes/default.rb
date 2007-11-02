31a9303271db1a8c2193bd43b17ec665c196b76f
Mark Van Holstyn <mvanholstyn@gmail.com>
2007-11-02 15:42:12 +0000

    Update template path

--- recipes/default.rb
+++ recipes/default.rb
@@ -143,7 +143,7 @@ namespace :deploy do
     
     # Copy over database.yml template
     run "umask 02 && mkdir -p #{shared_path}/config"
-    template = File.read(File.join(File.dirname(__FILE__), "templates", "config", "database.yml"))
+    template = File.read(File.join(File.dirname(__FILE__), "..", "templates", "config", "database.yml"))
     result = ERB.new(template).result(binding)
     put result, "#{shared_path}/config/database.yml", :mode => 0644
     
@@ -153,7 +153,7 @@ namespace :deploy do
     
     # Copy over a virtual host config file
     # run "umask 02 && mkdir -p /etc/apache2/sites-available /etc/apache2/sites-enabled"
-    # template = File.read(File.join(File.dirname(__FILE__), "templates", "config", "virtual_host.conf"))
+    # template = File.read(File.join(File.dirname(__FILE__), "..", "templates", "config", "virtual_host.conf"))
     # result = ERB.new(template).result(binding)
     # put result, "/etc/apache2/sites-available/#{application}-#{rails_env}.conf", :mode => 0644
     # run "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf && ln -s /etc/apache2/sites-available/#{application}-#{rails_env}.conf /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf"
