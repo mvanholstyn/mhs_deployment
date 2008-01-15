3d88077e7cbdf8e3d3cf3ce93cdb3d60c6fae68b
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-01-15 15:25:08 +0000

    Scrap setup_extras task, just hook each task to deploy:setup

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,14 +1,2 @@
-namespace :deploy do
-  task :setup_extras, :except => { :no_release => true } do
-    db.configure
-    mongrel.cluster.configure
-    
-    # Copy over a virtual host config file
-    # run "umask 02 && mkdir -p /etc/apache2/sites-available /etc/apache2/sites-enabled"
-    # template = File.read(File.join(File.dirname(__FILE__), "..", "templates", "config", "virtual_host.conf"))
-    # result = ERB.new(template).result(binding)
-    # put result, "/etc/apache2/sites-available/#{application}-#{rails_env}.conf", :mode => 0644
-    # run "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf && ln -s /etc/apache2/sites-available/#{application}-#{rails_env}.conf /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf"
-  end
-  after "deploy:setup", "deploy:setup_extras"
-end
+after "deploy:setup", "db:configure"
+after "deploy:setup", "mongrel:cluster:configure"
