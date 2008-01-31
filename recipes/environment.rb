28a49fbef94440e527ae221b4d1d770b0db507d5
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-01-31 03:36:39 +0000

    Add localhost task to easily deploy to localhost

--- recipes/environment.rb
+++ recipes/environment.rb
@@ -5,6 +5,14 @@
   end
 end
 
+desc "Setup deployment to localhost"
+task :localhost do
+  role :web, "localhost"
+  role :app, "localhost"
+  role :db, "localhost", :primary => true
+  set :rails_env, :production
+end
+
 Dir[File.join(File.dirname(__FILE__), *%w[.. .. .. .. config deploy *.rb])].each do |deploy_file|
   task File.basename(deploy_file).gsub(".rb", "") do
     load deploy_file
