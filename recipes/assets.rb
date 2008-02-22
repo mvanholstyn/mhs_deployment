4f0c7663db9bc2f53d708f3a067f3bc99eda2355
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-02-22 17:16:03 +0000

    Fix small typo

--- recipes/assets.rb
+++ recipes/assets.rb
@@ -9,7 +9,7 @@ namespace :assets do
     desc "Restores a backup of the assets."
     task :restore, :roles => :db, :only => {:primary => true} do
       ENV['BACKUP_DIR'] ||= "#{shared_path}/backups"
-      run "cd #{current_path} && rake #{rails_env} asset:backup:restore BACKUP_DIR=#{ENV['BACKUP_DIR']} BACKUPS=#{backups.join(',')}"
+      run "cd #{current_path} && rake #{rails_env} assets:backup:restore BACKUP_DIR=#{ENV['BACKUP_DIR']} BACKUPS=#{backups.join(',')}"
     end
   end
 end
