8906f55f2bc2854e0255855fdb83b18b4025a299
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-10-03 13:19:19 -0400

    Don't need Etc anymore since we aren't using it to set the svn user. Just set rails env to staging, dont use ENV config. Set branch with -s/-S instead of ENV config. Deploy to /home/user/application/rails_env/branch

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,14 +1,4 @@
-require 'etc'
 # You must set :application, :repository, and :server in your Capfile
-
-# Sets the RAILS_ENV for this deployment. Default: staging
-set(:rails_env, ENV["RAILS_ENV"] ? ENV["RAILS_ENV"].to_sym : :staging)
-
-if branch = ENV["BRANCH"]
-  set(:branch, branch)
-end
-
-set(:deploy_via, :remote_cache)
 
 # Sets the server. This MUST be set.
 # set(:server) { abort "Please specify the server to deploy to, set :server, 'example.com'" }
@@ -16,18 +6,12 @@ set(:deploy_via, :remote_cache)
 # role(:app)                  { server }
 # role(:db, :primary => true) { server }
 
-# Sets the user to deploy as. Default: #{application}
+set(:rails_env, :staging)
+set(:scm, :git)
+set(:deploy_via, :remote_cache)
 set(:user) { application }
-
-set :scm, :git
-
-# Sets the location to deploy to. Default: /var/www/#{application}/#{rails_env}
-set(:deploy_to) { "/var/www/#{application}/#{rails_env}" }
-
-# Sets the extra paths to symlink.
+set(:deploy_to) { "/home/#{user}/#{application}/#{rails_env}/#{branch}" }
 set(:symlinks, [])
-
 set(:backups, [])
 set(:backup_on_deploy, true)
-
 set(:use_sudo, false)
