658ee9c7ab83b50b3d1aec1a1c4ad5647cd42adf
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-11-25 13:59:32 -0500

    Name the apache config application-rails_env-branch. Update templates for apache config (store logs locally) and for database (new parameters)

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -4,7 +4,7 @@ namespace :apache do
     run "umask 02 && mkdir -p #{shared_path}/config"
     template = File.read(File.join(File.dirname(__FILE__), "..", "templates", "config", "virtual_host.conf"))
     result = ERB.new(template).result(binding)
-    put result, "#{shared_path}/config/virtual_host.conf", :mode => 0644
-    # sudo "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf && ln -s #{shared_path}/config/virtual_host.conf /etc/apache2/sites-enabled/#{application}-#{rails_env}.conf"
+    put result, "#{shared_path}/config/#{application}-#{rails_env}-#{branch}.conf", :mode => 0644
+    # sudo "rm -f /etc/apache2/sites-enabled/#{application}-#{rails_env}-#{branch}.conf && ln -s #{shared_path}/config/#{application}-#{rails-env}-#{branch}.conf /etc/apache2/sites-enabled/#{application}-#{rails-env}-#{branch}.conf"
   end
 end
