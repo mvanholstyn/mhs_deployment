a4d2d4616c046090046b94f6ab8c1d8dcd573ff0
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-02-07 12:05:15 -0500

    dont try to get the rake variable

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -1,6 +1,6 @@
 namespace :gems do
   task :build do
-    run "cd #{release_path} && #{rake} #{rails_env} gems:build"
+    run "cd #{release_path} && rake #{rails_env} gems:build"
   end
 end
 after "deploy:update_code", "gems:build"
